ce728579646257a08d3094a2a7363ddcff907689
chaseadams509 <chase.adams509@gmail.com>
2020-06-28 10:26:19 -0700

    Add moving enemy (#8)
    * Add moving enemy
    
    * Use Prefab and have 2 enemies

---
+++ GMTK Game Jam 2020/Assets/Scripts/OpposomBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpposomBehaviourScript : MonoBehaviour
+{
+    public Vector2 velocity = new Vector2(-1.0f, 0.0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 newPosition = currentPosition + velocity * Time.deltaTime;
+
+        transform.position = newPosition;
+    }
+}
