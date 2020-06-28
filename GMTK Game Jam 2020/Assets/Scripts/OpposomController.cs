f9965be368343947f41d1853e34c3bcc7da1fc67
chaseadams509 <chase.adams509@gmail.com>
2020-06-28 11:05:45 -0700

    Adds collision, health, and death

--- GMTK Game Jam 2020/Assets/Scripts/OpposomController.cs
+++ GMTK Game Jam 2020/Assets/Scripts/OpposomController.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpposomBehaviourScript : MonoBehaviour
+public class OpposomController : MonoBehaviour
 {
     public Vector2 velocity = new Vector2(-1.0f, 0.0f);
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Starting Opposom");
     }
 
     // Update is called once per frame
@@ -19,4 +19,17 @@ public class OpposomBehaviourScript : MonoBehaviour
 
         transform.position = newPosition;
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Hit Something!");
+        //Checks if other gameobject has a Tag of Player
+        if (other.gameObject.tag == "Player")
+        {
+            print("Found player");
+            other.gameObject.GetComponent<PlayerController>().health -= 1;
+            Time.timeScale = 0;
+        }
+    }
+
 }
---
+++ GMTK Game Jam 2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 1; // Only 1 life for now
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            print("He ded");
+            transform.position = new Vector2(-20.0f, -20.0f);
+            health = -1;
+        }
+    }
+}
