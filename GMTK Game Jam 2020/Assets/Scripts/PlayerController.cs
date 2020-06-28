3406011801d274727c821405b830462af13066cf
chaseadams509 <chase.adams509@gmail.com>
2020-06-28 11:37:16 -0700

    Add Game Over title

--- GMTK Game Jam 2020/Assets/Scripts/PlayerController.cs
+++ GMTK Game Jam 2020/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int health;
+    private GameObject[] gameOverObjects;
     // Start is called before the first frame update
     void Start()
     {
         health = 1; // Only 1 life for now
+        gameOverObjects = GameObject.FindGameObjectsWithTag("OnGameOver");
+        foreach (GameObject obj in gameOverObjects)
+        {
+            obj.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -19,6 +25,10 @@ public class PlayerController : MonoBehaviour
             print("He ded");
             transform.position = new Vector2(-20.0f, -20.0f);
             this.gameObject.SetActive(false);
+            foreach (GameObject obj in gameOverObjects)
+            {
+                obj.SetActive(true);
+            }
             health = -1;
         }
     }
