63678eb669f03e50c59119aadbb6efa2ff6e79c5
chaseadams509 <chase.adams509@gmail.com>
2020-07-11 10:51:58 -0700

    Spawns Bots (#16)
    ctrl Spawns red bots, alt spawns blue, shift spawns green.

---
+++ GMTK Game Jam 2020/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyPrefab;
+    ObjectPooler enemyPooler;
+    void Start()
+    {
+        enemyPooler.objectToPool = enemyPrefab;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GMTK Game Jam 2020/Assets/Scripts/PlayerMovement.cs
+++ GMTK Game Jam 2020/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public GameObject botPrefab;
+    ObjectPooler botPooler;
     Vector3 pos;
     Rigidbody2D rb;
     Vector2 movement;
@@ -13,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody2D>();
+        botPooler = new ObjectPooler(botPrefab);
     }
 
     // Update is called once per frame
@@ -27,6 +30,33 @@ public class PlayerMovement : MonoBehaviour
         pos.y = Mathf.Clamp(pos.y, 0.02f, 0.98f);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            CreateBot(BotBehavior.BehaviorType.Red);
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            CreateBot(BotBehavior.BehaviorType.Blue);
+        }
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            CreateBot(BotBehavior.BehaviorType.Green);
+        }
+    }
+
+    void CreateBot(BotBehavior.BehaviorType bType)
+    {
+        GameObject newBot = botPooler.GetPooledObject();
+        if (newBot != null)
+        {
+            Vector3 offset = new Vector3(0.5f, 0.5f, 0f);
+            newBot.transform.position = transform.position + offset;
+            BotBehavior newBotScript = newBot.GetComponent<BotBehavior>();
+            newBotScript.behaviorType = bType;
+            newBot.SetActive(true);
+        }
     }
 
 
