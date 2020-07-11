170fb5c0c03d4f5df5aca3573dd2aa28753cfbc4
chaseadams509 <chase.adams509@gmail.com>
2020-07-11 12:01:30 -0700

    Enemies spawn
    Currently set to every second, and only red ones.

---
+++ GMTK Game Jam 2020/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    ObjectPooler enemyPooler;
+    int wave;
+    private float waitTime = 1.0f;
+    private float timer = 0.0f;
+    TextMeshProUGUI waveText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyPooler = new ObjectPooler(enemyPrefab);
+        wave = 0;
+        waveText = this.gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > waitTime)
+        {
+            timer = timer - waitTime;
+            wave += 1;
+            waveText.SetText("Wave: " + wave);
+            SpawnEnemies();
+        }
+
+    }
+
+    void SpawnEnemies()
+    {
+        if (wave == 1)
+        {
+            Vector3 initialLocation = new Vector3(-3f, -1f);
+            SpawnEnemy(initialLocation);
+        } else if (wave % 2 == 0)
+        {
+            int numEnemies = (int)Mathf.Log(wave, 2f) + 1;
+            for (int i = 0; i < numEnemies; i++)
+            {
+                float angle = 2.0f * Mathf.PI * Random.Range(0f, 1f);
+                Vector3 initialLocation = new Vector3(3 * Mathf.Cos(angle), 3 * Mathf.Sin(angle));
+                SpawnEnemy(initialLocation);
+            }
+        } else
+        {
+            int numEnemies = wave / 2;
+            for (int i = 0; i < numEnemies; i++)
+            {
+                float angle = 2.0f * Mathf.PI * Random.Range(0f, 1f);
+                Vector3 initialLocation = new Vector3(3 * Mathf.Cos(angle), 3 * Mathf.Sin(angle));
+                SpawnEnemy(initialLocation);
+            }
+
+        }
+    }
+
+    void SpawnEnemy(Vector3 startLocation)
+    {
+        GameObject newEnemy = enemyPooler.GetPooledObject();
+        if (newEnemy != null)
+        {
+            newEnemy.transform.position = startLocation;
+            EnemyBehavior newEnemyScript = newEnemy.GetComponent<EnemyBehavior>();
+            newEnemyScript.behaviorType = EnemyBehavior.EnemyType.Red;
+            newEnemy.SetActive(true);
+        }
+    }
+}
