81105ff72fecf52772214f9169f3149479911d36
chaseadams509 <chase.adams509@gmail.com>
2020-07-11 17:26:26 -0700

    Add Points pickups

---
+++ GMTK Game Jam 2020/Assets/Scripts/PointsPickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsPickup : MonoBehaviour
+{
+    public GameObject gameControllerInstance;
+    public int points;
+    public Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Current Position is " + transform.position);
+        transform.position = position;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collidedObj = collision.gameObject;
+        switch (collidedObj.tag)
+        {
+            case "Player":
+                this.gameObject.SetActive(false);
+                this.gameControllerInstance.GetComponent<GameController>().AddScore(points);
+                break;
+            case "Enemy":
+                this.gameObject.SetActive(false);
+                break;
+        }
+    }
+}
